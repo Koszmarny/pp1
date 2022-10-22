b101816c9fce65982c3fce8563b1a13e274a1a82
Paweł Byszewski <pawelby2@gmail.com>
2022-10-22 12:20:38 +0200

    zad 14

---
+++ 03-ControlStructures/Zadanie 10.py
@@ -0,0 +1,7 @@
+i = 0
+while i < 5:
+  print("Practice makes perfect")
+  i += 1
+
+for j in range(5): print("Practice makes perfect")
+#range(poczatek, koniec+1, o  ile zmieniac)
---
+++ 03-ControlStructures/Zadanie 12.py
@@ -0,0 +1,4 @@
+j=0
+for i in range(100, 151, 1):
+    j = j + i
+print (j)    
