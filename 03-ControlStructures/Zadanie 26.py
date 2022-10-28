c7ab749ab7974dc091e2764a8bab05d3977a2074
Paweł Byszewski <pawelby2@gmail.com>
2022-10-28 15:56:13 +0200

    ser

--- 03-ControlStructures/Zadanie 26.py
+++ 03-ControlStructures/Zadanie 26.py
@@ -1,16 +1,10 @@
-from pickle import FALSE, TRUE
-from signal import pause
-from tkinter import END
-
-
 PIN = "0805"
 
-def check():
-    x = input("Enter PIN code: ")
-    if x==PIN: return TRUE
-    else: print("Incorrect...")
-
 for i in range(1,4,1):
-    check()
-    if TRUE: break
-    else: i=i+1
+    x = input("Enter PIN code: ")
+    if x==PIN: 
+        print("poprawne haslo") 
+        break
+    else: 
+        print("Niepoprawne...")
+        i=i+1
---
+++ 03-ControlStructures/Zadanie 28.py
@@ -0,0 +1,7 @@
+
+a,b=0,1
+print(a, end=" ")
+for i in range(1,29,1):
+    print(b,end=" ")
+    a,b=b,a+b
+    
