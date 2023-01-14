e64e77428cc0c00dda20c32d215e76375a508478
Paweł Byszewski <pawelby2@gmail.com>
2023-01-14 16:59:32 +0100

    z10

--- 09-Test2/p1.py
+++ 09-Test2/p1.py
@@ -1,18 +1,23 @@
 def f(player1, player2):
-    cards={"A","K","Q","J","T"}
-    sum1=0
-    sum2=0
+    cards = {"A", "K", "Q", "J", "T"}
+    sum1 = 0
+    sum2 = 0
     for number in player1:
         if number in cards:
-            sum1+=10
-        else: sum1+=int(number)
+            sum1 += 10
+        else:
+            sum1 += int(number)
     for number in player2:
         if number in cards:
-            sum2+=10
-        else: sum2 +=int(number)
-    if sum1>sum2: return True
-    else: return False
+            sum2 += 10
+        else:
+            sum2 += int(number)
+    if sum1 > sum2:
+        return True
+    else:
+        return False
 
-print(f("AJ972","AQT72"))
-print(f("9532","K8"))
-print(f("987","AT4"))
+
+print(f("AJ972", "AQT72"))
+print(f("9532", "K8"))
+print(f("987", "AT4"))
---
+++ 10-ObjectOrientedProgramming/Zad8.py
@@ -0,0 +1,12 @@
+class TV():
+    def __init__(self):
+        self.is_on()=False
+    def turn_on(self):
+        self.is_on()=True
+    def turn_off(self):
+        self.is_on()=False
+
+
+
+
+print(TV.is_on())
